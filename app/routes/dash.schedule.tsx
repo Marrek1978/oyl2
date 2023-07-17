6b98df2ab9f100a425e1248c40fc6fa3ab12fe75
Mark <103870627+Marrek1978@users.noreply.github.com>
2023-07-17 15:18:55 -0600

    outside calendar dnd working - basic

--- app/routes/dash.schedule.tsx
+++ app/routes/dash.schedule.tsx
@@ -4,7 +4,7 @@ import type { Event } from 'react-big-calendar'
 // EventProps } from 'react-big-calendar'
 import moment from 'moment'
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
-import type { EventInteractionArgs } from 'react-big-calendar/lib/addons/dragAndDrop'
+import type { EventInteractionArgs, DragFromOutsideItemArgs } from 'react-big-calendar/lib/addons/dragAndDrop'
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
 import PropTypes from 'prop-types'
 
@@ -13,58 +13,134 @@ import events from '~/components/Calendar/events/events'
 const localizer = momentLocalizer(moment)
 const DragAndDropCalendar = withDragAndDrop(Calendar)
 
+//example from https://github.com/jquense/react-big-calendar/blob/master/stories/demos/exampleCode/dndOutsideSource.js
 
-//*  set up dnd from outside calendar
+//* load events from db
 // * set up object shapes for importing todos/habits, etc, convert to events, for dnd
-// * set up dnd from outside calendar
+//* remove non-essentials - formatName, counters, etc
+//* set proper default date
+//* whole isdraggable?? props, etc.
 //* set up db for saving events 
 //* consider how to structure and sync saved events vs converted events
 
 
+//  type for events with id field
 interface myEvent extends Event {
   id: number;
 }
 
+// interface draggableEvent extends Event {
+//   isDraggable: boolean;
+// }
+
+type DragItem = { title: string; name: string; } | 'undroppable';
+// export interface Event {
+//   allDay?: boolean | undefined;
+//   title?: React.ReactNode | undefined;
+//   start?: Date | undefined;
+//   end?: Date | undefined;
+//   resource?: any;
+// }
+
 //!  events will eventually come from lists/todos/habits, etc... and be converted to draggable events
-// const events: myEvent[] = [
-//   {
-//     id: 0,
-//     title: 'Board meeting',
-//     start: new Date(2018, 0, 29, 9, 0, 0),
-//     end: new Date(2018, 0, 29, 13, 0, 0),
-//     resource: [1],
-//   },
-//   {
-//     id: 1,
-//     title: 'MS training',
-//     allDay: true,
-//     start: new Date(2018, 0, 29, 14, 0, 0),
-//     end: new Date(2018, 0, 29, 16, 30, 0),
-//     resource: [2],
-//   },
-//   {
-//     id: 2,
-//     title: 'Team lead meeting',
-//     start: new Date(2018, 0, 29, 8, 30, 0),
-//     end: new Date(2018, 0, 29, 12, 30, 0),
-//     resource: [2, 3],
-//   },
-//   {
-//     id: 11,
-//     title: 'Birthday Party',
-//     start: new Date(2018, 0, 30, 7, 0, 0),
-//     end: new Date(2018, 0, 30, 10, 30, 0),
-//     resource: [4],
-//   },
-// ]
+const adjEvents = events.map((event, index) => ({
+  ...event,
+  // isDraggable: index % 2 === 0,
+  isDraggable: true,
+}))
+
+//label for draggable events
+//!  titles will come from list of loaded data
+const formatName = (name: string, count: number) => `${name} ID ${count}`
+
+
 
 function Schedule() {
 
-  const [myEvents, setMyEvents] = useState<myEvent[]>(events)
+  const [myEvents, setMyEvents] = useState<myEvent[]>(adjEvents)
+  const [draggedEvent, setDraggedEvent] = useState<DragItem>()
+  // const [displayDragItemInCell, setDisplayDragItemInCell] = useState<boolean>(true)
+  // const displayDragItemInCell = true
+
+  const [counters, setCounters] = useState<{ [key: string]: number }>({ item1: 0, item2: 0 })
+
   const defaultDate = useMemo(() => new Date(2015, 3, 12), [])
 
+  const eventPropGetter = useCallback(
+    (event: object) => {
+      if ('isDraggable' in event && event.isDraggable) {
+        return { className: 'isDraggable' }
+      } else {
+        return { className: 'isNotDraggable' }
+      }
+    }, [])
+
+  const handleDragStart = useCallback((draggedItem: DragItem) => setDraggedEvent(draggedItem), [])
+
+  // const dragFromOutsideItem = useCallback(() => draggedEvent, [draggedEvent])
+  const dragFromOutsideItem = useCallback(() => {
+    return (event: object) => {
+      if (draggedEvent !== 'undroppable' && draggedEvent !== undefined) {
+        return new Date();
+      }
+      return new Date(0)
+    }
+  }, [draggedEvent])
+
+  const customOnDragOver = useCallback(
+    (dragEvent: React.DragEvent<Element>) => {
+      if (draggedEvent !== 'undroppable') {
+        console.log('preventDefault')
+        dragEvent.preventDefault()
+      }
+    }, [draggedEvent])
+
+  //!   could be fixed to true... no need for assoc. input
+  // const handleDisplayDragItemInCell = useCallback(() => setDisplayDragItemInCell((prev) => !prev), [])
+
+  const newEvent = useCallback((event: Event): void => {
+    setMyEvents((prev) => {
+      const idList = prev.map((item) => item.id)
+      const newId = Math.max(...idList) + 1
+      return [...prev, { ...event, id: newId, isDraggable: true }]
+    })
+  }, [setMyEvents])
+
+
+  const onDrdopFromOutside = useCallback((
+    { start: startDate, end: endDate, allDay: isAllDay = false }: DragFromOutsideItemArgs
+  ) => {
+    if (draggedEvent === undefined) return
+    if (draggedEvent === 'undroppable') {
+      setDraggedEvent(undefined)
+      return
+    }
+
+    const start = typeof startDate === 'string' ? new Date(startDate) : startDate;
+    const end = typeof endDate === 'string' ? new Date(endDate) : endDate;
+
+    const { name } = draggedEvent
+    const event = {
+      title: formatName(name, counters[name]),
+      start,
+      end,
+      isAllDay,
+    }
+    setDraggedEvent(undefined)
+    setCounters((prev) => {
+      const { [name]: count } = prev
+      return {
+        ...prev,
+        [name]: count + 1,
+      }
+    })
+    newEvent(event)
+
+  }, [draggedEvent, counters, setDraggedEvent, setCounters, newEvent])
+
+
   const moveEvent = useCallback((
-    { event, start, end, isAllDay: droppedOnAllDaySlot }: EventInteractionArgs<any>
+    { event, start, end, isAllDay: droppedOnAllDaySlot = false }: EventInteractionArgs<any>
   ): void => {
 
     const { allDay } = event
@@ -105,16 +181,50 @@ function Schedule() {
         Drag and Drop an "event" from one slot to another to "move" the event,
         or drag an event's drag handles to "resize" the event.
       </strong>
+
+      <div className='card' >
+        <div className="inner">
+          <h4>Outside Drag Sources</h4>
+          <p>
+            Lighter colored events, in the Calendar, have an `isDraggable` key
+            of `false`.
+          </p>
+          {Object.entries(counters).map(([name, count]) => (
+            <div
+              draggable="true"
+              className='border-2 border-gray-400 p-2 bg-slate-200'
+              key={name}
+              onDragStart={() =>
+                handleDragStart({ title: formatName(name, count), name })
+              }
+            >
+              {formatName(name, count)}
+            </div>
+          ))}
+          <div
+            draggable="true"
+            onDragStart={() => handleDragStart('undroppable')}
+          >
+            Draggable but not for calendar.
+          </div>
+        </div>
+      </div>
       <div className="h-[600px]">
         <DragAndDropCalendar
           defaultDate={defaultDate}
-          defaultView={Views.MONTH}
+          defaultView={Views.WEEK}
+          dragFromOutsideItem={dragFromOutsideItem}
+          draggableAccessor={(event: any) => event.isDraggable}
+          eventPropGetter={eventPropGetter}
           events={myEvents}
           localizer={localizer}
+          onDropFromOutside={onDrdopFromOutside}
+          onDragOver={customOnDragOver}
           onEventDrop={moveEvent}
           onEventResize={resizeEvent}
-          popup
+          onSelectSlot={newEvent}
           resizable
+          selectable
         />
       </div>
     </>
