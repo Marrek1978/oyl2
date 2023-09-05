ca92cabadec7e665ece5ec500bd951dc30e15e3a
Mark <103870627+Marrek1978@users.noreply.github.com>
2023-09-05 13:23:16 -0600

    projects on schedule

--- app/components/nav/SchedulerProjectsNavAndDraggables.tsx
+++ app/components/nav/SchedulerProjectsNavAndDraggables.tsx
@@ -1,4 +1,10 @@
-import { NavLink } from '@remix-run/react'
+import { useEffect, useState } from 'react'
+
+import SubHeading12px from '../titles/SubHeading12px'
+import SubHeading14px from '../titles/SubHeading14px'
+
+import type { ListAndToDos } from '~/types/listTypes'
+import type { RoutineAndToDos } from '~/types/routineTypes'
 import type { ProjectWithListsAndRoutines } from '~/types/projectTypes'
 
 
@@ -6,55 +12,136 @@ type Props = {
   projectsWithListsAndRoutines: ProjectWithListsAndRoutines[]
 }
 
+
 function SchedulerProjectsNavAndDraggables({ projectsWithListsAndRoutines }: Props) {
 
+  // const [selectedProjectId, setSelectedProjectId] = useState('')
   const projects: ProjectWithListsAndRoutines[] = projectsWithListsAndRoutines
+  const [specificProject, setSpecificProject] = useState<ProjectWithListsAndRoutines>()
+
+  const [specificProjectLists, setSpecificiProjectLists] = useState<ListAndToDos[]>()
+  const [specificProjectRoutines, setSpecificiProjectRoutines] = useState<RoutineAndToDos[]>()
   // console.log('projecsts are ', projects)
+
+
+  useEffect(() => {
+    const project = projects.find((project: ProjectWithListsAndRoutines) => project.sortOrder === 0)
+    project && setSpecificProject(project)
+    project?.lists && setSpecificiProjectLists(project.lists)
+    project?.routines && setSpecificiProjectRoutines(project.routines)
+
+  }, [projects])
+
+
+  useEffect(() => {
+    console.log('specificProject is ', specificProject)
+  }, [specificProject])
+
+
+  const handleProjectsNavClick = (id: string) => {
+    console.log('id is ', id)
+    // setSelectedProjectId(id)
+    const project = projects.find((project: ProjectWithListsAndRoutines) => project.id === id)
+    if (project) {
+      setSpecificProject(project)
+    }
+  }
+
+
 
   return (
     <>
-      <nav >
-        <ul id='side-nav2' className="menu w-56 p-0 [&_li>*]:rounded-none">
+      <div className='flex gap-8'>
+
+        {/* cliclable list of projects*/}
+        <nav >
+          <ul className="menu w-56 p-0 [&_li>*]:rounded-none">
+
+            {/* //!  , active styles  */}
+
+            {projects.map((project, index) =>
+              project.sortOrder === 0 ? (
+                <li key={project.id}
+                  className='scheduler-projects-nav  border-2 border-success '
+                  onClick={() => handleProjectsNavClick(project.id)}
+                >
+                  <div className='p-0 block hover:bg-base-100'>
+                    <div className='text-success font-bold'>
+                      <SubHeading12px text={'Main Focus'} />
+                    </div>
+                    <div className='  '>
+                      {project.title}
+                    </div>
+                  </div>
+                </li>
+              ) : (
+                <li key={project.id}
+                  className='scheduler-projects-nav '
+                  onClick={() => handleProjectsNavClick(project.id)}
+                >
+                  {project.title}
+                </li>
+              )
+            )}
+
+          </ul>
+        </nav>
 
 
-// ! make into buttons
-//! sortOrder = 0 ==> special styles 
-//! hover, active styles 
-//!  state to open 2nd menu 
 
-//!  styles for2nd menu, different again for sortOrder = 0
-          {projects.map((project, index) => (
+        {/* Draggable list of project deliversagbles */}
 
-            <li key={project.id} className="">
-              <NavLink to={``} end >
-                <span>{project.title}</span>
-                
-              </NavLink>
-            </li>
-          ))}
+        {specificProject && (
 
-          <li className="">
-            <NavLink to='/dashboard/focus/daily/priorities/projects' end >
-              <span>Today</span>
-            </NavLink>
-          </li>
-
-          <li className="">
-            <NavLink to='/dash/schedule' end >
-              <span>Schedule</span>
-            </NavLink>
-          </li>
+          <nav >
+            <ul className="menu w-56 p-0 [&_li>*]:rounded-none text-left">
 
 
+              {/* project time block */}
 
-          <li className="">
-            <NavLink to='/dash/alllists' >
-              <span>Lists & Routines</span>
-            </NavLink>
-          </li>
+              <li key={specificProject.id}
+                className='scheduler-projects-nav '
+              >
+                <SubHeading14px text={'Time Block for'} />
+                {specificProject.title}
+              </li>
 
-        </ul>
-      </nav>
+              {/*  
+               
+               //!  , active styles 
+               //!  state to open 2nd menu 
+               
+              //!  styles for2nd menu, different again for sortOrder = 0 */}
+
+              {projects.map((project, index) =>
+                project.sortOrder === 0 ? (
+                  <li key={project.id}
+                    className='scheduler-projects-nav  border-2 border-success '
+                    onClick={() => handleProjectsNavClick(project.id)}
+                  >
+                    <div className='p-0 block hover:bg-base-100'>
+                      <div className='text-success font-bold'>
+                        <SubHeading12px text={'Main Focus'} />
+                      </div>
+                      <div className='  '>
+                        {project.title}
+                      </div>
+                    </div>
+                  </li>
+                ) : (
+                  <li key={project.id}
+                    className='scheduler-projects-nav '
+                    onClick={() => handleProjectsNavClick(project.id)}
+                  >
+                    {project.title}
+                  </li>
+                )
+              )}
+
+            </ul>
+          </nav>
+        )}
+      </div>
     </>
   )
 }
