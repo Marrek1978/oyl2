cea17ced78661c80e147a390916bbf93a4954972
Mark <103870627+Marrek1978@users.noreply.github.com>
2023-09-12 16:21:22 -0600

    DesiresDND styling

--- app/components/dnds/desires/DndSortableDesire.tsx
+++ app/components/dnds/desires/DndSortableDesire.tsx
@@ -47,7 +47,6 @@ function DndSortableDesire({ id, title, linkTitle = 'Edit', desireValues = [], d
             hover:text-primary-focus
             max-w-prose
           '>
-
           <H2WithLink
             title={title}
             linkDestination={id}
@@ -55,51 +54,48 @@ function DndSortableDesire({ id, title, linkTitle = 'Edit', desireValues = [], d
           />
 
           {hasValues && (
-
-            <div className="flex flex-wrap gap-2 items-center mt-1 font-bold ">
+            <div className="grid grid-cols-[130px_1fr] gap-2 items-start mt-1 font-bold ">
               <div className='text-secondary/70'>
                 <SubHeading12px text={'Serves Values:'} />
               </div>
-              {desireValues?.map((value) => {
-                const title = value.value.valueTitle
-                let id = uuidv4();
-                return (
-                  <div key={id}
-                    className={`
-                font-bold
-                text-secondary
-                `} >
-                    <SubHeading12px
-                      text={`${title}, `}
-                    />
-                  </div>
-                )
-              })
-              }
+              <div className='flex flex-wrap gap-2'>
+                {desireValues?.map((value) => {
+                  const title = value.value.valueTitle
+                  let id = uuidv4();
+                  return (
+                    <div key={id}
+                      className={`
+                        font-bold
+                        text-secondary
+                    `} >
+                      <SubHeading12px text={`${title}, `} />
+                    </div>
+                  )
+                })}
+              </div>
             </div>
           )}
 
           {hasOutcomes && (
-            <div className="flex flex-wrap gap-2 items-center mt-1 font-bold ">
+            <div className="grid grid-cols-[130px_1fr] gap-2 items-start mt-1 font-bold ">
               <div className='text-base-content/70 '>
                 <SubHeading12px text={'Withs Outomes:'} />
               </div>
-              {desireOutcomes?.map((outcome) => {
-                const title = outcome.title
-                let id = uuidv4();
-                return (
-                  <div key={id}
-                    className={`
-                    font-bold
-                    text-base-content
-                  `} >
-                    <SubHeading12px
-                      text={`${title}, `}
-                    />
-                  </div>
-                )
-              })
-              }
+              <div className='flex flex-wrap gap-2'>
+                {desireOutcomes?.map((outcome) => {
+                  const title = outcome.title
+                  let id = uuidv4();
+                  return (
+                    <div key={id}
+                      className={`
+                      font-bold
+                      text-base-content
+                    `} >
+                      <SubHeading12px text={`${title}, `} />
+                    </div>
+                  )
+                })}
+              </div>
             </div>
           )}
 
